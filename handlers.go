f1cbbff983a77e8a73e4f64cd6c4451aa87f4d92
Vanessa Schissato <vanessaschissato@gmail.com>
2016-09-05 19:10:44 -0300

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -10,7 +10,7 @@ func Authentication(w http.ResponseWriter, r *http.Request) {
 	user := User{
 		Email: "vanessaschissato@gmail.com",
 		Name:  "Vanessa Schissato",
-		Phone: "+55 (11) 9 4187-8106",
+		Phone: "+55 (11) 9 4321-1234",
 	}
 
 	json.NewEncoder(w).Encode(user)
